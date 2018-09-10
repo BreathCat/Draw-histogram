591eca1cd5b22f25aa5d210ae173019e08233ce0
BreathCat <454520360@qq.com>
2018-09-10 22:55:15 +0800

    Y鐨勪竴鏍肩殑闀垮害

--- data_analyze.m
+++ data_analyze.m
@@ -1,10 +1,10 @@
 
-ymax = 2*10^5;
+ymax = 2.5*10^5;
 LineWidth=1.2;
-Ori_data_temp=importdata('L:\TXT\P_PUNum\Ori_1_ducks_take_off_420_720p50_.bin_P_PUNum.txt');
-Henc_data_temp=importdata('L:\TXT\P_PUNum\Henc_1_ducks_take_off_420_720p50.bin_P_PUNum.txt');
-Enc_Ori_data_temp=importdata('L:\TXT\P_PUNum\Enc_Ori_1_ducks_take_off_420_720p50.bin_P_PUNum.txt');
- Enc_Henc_data_temp=importdata('L:\TXT\P_PUNum\Enc_Henc_1_ducks_take_off_420_720p50.bin_P_PUNum.txt');
+Ori_data_temp=importdata('L:\TXT\P_PUNum\Ori_3_Tennis_1920x1080_24.bin_P_PUNum.txt');
+Henc_data_temp=importdata('L:\TXT\P_PUNum\Henc_3_Tennis_1920x1080_24.bin_P_PUNum.txt');
+Enc_Ori_data_temp=importdata('L:\TXT\P_PUNum\Ori_3_Tennis_1920x1080_24.bin_P_PUNum.txt');
+Enc_Henc_data_temp=importdata('L:\TXT\P_PUNum\Enc_Henc_Tennis_3_24.bin_P_PUNum.txt');
 % Enc_Henc_data_temp=importdata('L:\TXT\P_PUNum\Enc_Henc_4_decducks_take_off_420_720p50_.bin_P_PUNum.txt');
 % [m,n]=size(Double_data); %m=25(I_frame)   n=35(intra_pre_modes)
 Ori_data = sum(Ori_data_temp);
@@ -27,6 +27,7 @@ Enc_Henc_data = sum(Enc_Henc_data_temp);
  xlim([0, 25]);%只设定x轴的绘制范围
  ylim([0, ymax]);%只设定y轴的绘制范围
  set(gca,'XTick',0:5:25) %改变x轴坐标间隔显示 这里间隔为2
+ set(gca,'YTick',0:ymax/5:ymax) %改变y轴坐标间隔显示 这里间隔为2
 
  subplot(2,2,2);
  plot(Ori_data,'g:*','LineWidth',LineWidth);
@@ -37,7 +38,8 @@ Enc_Henc_data = sum(Enc_Henc_data_temp);
  xlim([0, 25]);%只设定x轴的绘制范围
  ylim([0, ymax]);%只设定y轴的绘制范围
  set(gca,'XTick',0:5:25) %改变x轴坐标间隔显示 这里间隔为2
-
+ set(gca,'YTick',0:ymax/5:ymax) %改变y轴坐标间隔显示 这里间隔为2
+ 
  subplot(2,2,3);
  plot(Henc_data,'m:*','LineWidth',LineWidth);
  hold on;
@@ -47,6 +49,7 @@ Enc_Henc_data = sum(Enc_Henc_data_temp);
  xlim([0, 25]);%只设定x轴的绘制范围
  ylim([0, ymax]);%只设定y轴的绘制范围
  set(gca,'XTick',0:5:25) %改变x轴坐标间隔显示 这里间隔为2
+ set(gca,'YTick',0:ymax/5:ymax) %改变y轴坐标间隔显示 这里间隔为2
 
  subplot(2,2,4);
  plot(Enc_Ori_data,'b:*','LineWidth',LineWidth);
@@ -57,7 +60,8 @@ Enc_Henc_data = sum(Enc_Henc_data_temp);
  xlim([0, 25]);%只设定x轴的绘制范围
  ylim([0, ymax]);%只设定y轴的绘制范围
  set(gca,'XTick',0:5:25) %改变x轴坐标间隔显示 这里间隔为2
- 
- suptitle('Duck-1');
-   saveas(gcf,'duck_1','tif');
-   saveas(gcf,'duck_1','fig');
+  set(gca,'YTick',0:ymax/5:ymax) %改变y轴坐标间隔显示 这里间隔为2
+
+ suptitle('Tennis-1920x1080-24—3');
+   saveas(gcf,'Tennis_1920x1080_24_3','tif');
+   saveas(gcf,'Tennis_1920x1080_24_3','fig');
